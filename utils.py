290a45531b75640b5329b9892a58bd4dc55c8834
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-11-18 15:02:58 +0600

    utilites

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import os
 import cv2
 
+from pathlib import Path
+
 def ensure_dir(file_path):
-    directory = os.path.dirname(file_path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    Path(file_path).mkdir(parents=True, exist_ok=True)
 
 def extract_frames(vid_path, out_folder):
     vidcap = cv2.VideoCapture(vid_path)
@@ -12,7 +12,7 @@ def extract_frames(vid_path, out_folder):
     count = 1
 
     while success:
-        out_path = os.path.join(out_folder, "frame%d.jpg" % count)
+        out_path = os.path.join(out_folder, "%03d.jpg" % count)
         cv2.imwrite(out_path, image)     # save frame as JPEG file
         success, image = vidcap.read()
         #print('Read a new frame: ', success)
