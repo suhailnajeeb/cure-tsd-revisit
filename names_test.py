b5f6937c97b981959350be691c56645aee0d3fc0
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-11-19 18:54:09 +0600

    testing names

---
+++ names_test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import os
+from utils import ensure_dir, get_names
+
+challenge = 9
+out_folder = "C:\\Data\\CURE-TSD\\output"
+
+
+srcnames, tgtnames = get_names(out_folder, challenge=9)
